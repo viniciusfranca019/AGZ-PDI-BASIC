8a5d7da7e0255d4b79f68ecdfa949b98251fceab
vinicius.franca <viniciusfrancas019@gmail.com>
2022-07-24 15:48:49 -0300

    add comment

--- Principios/calistenics/PeapleCollection.js
+++ Principios/calistenics/PeapleCollection.js
@@ -1,6 +1,8 @@
 // então vamos criar o objeto PeapleCollection, que irá ter agora
 // a função de lidar com as alterações que collection sofra
 
+// logo todos os metodos que exijam um tipo de iteração com a collection deverá ser feito através do peapleCollection
+
 const util = require('../Util/index');
 const { Person} = require('./Person')
 
